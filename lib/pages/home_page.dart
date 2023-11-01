4e5b46d29e4e1bc356c38e138b49cefef233ba99
Kirimach <kirimach102@gmail.com>
2023-11-01 18:52:43 +0700

    show room

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, use_build_context_synchronously, prefer_const_literals_to_create_immutables, avoid_print
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously, use_key_in_widget_constructors, library_private_types_in_public_api, avoid_print
 
 import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -10,17 +10,42 @@ final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
 void signOut(BuildContext context) async {
   await FirebaseAuth.instance.signOut();
-  // ทำการล้างค่าและนำผู้ใช้ออกจากระบบ
   Navigator.pushAndRemoveUntil(
     context,
-    MaterialPageRoute(
-        builder: (context) =>
-            LoginPage()), // ให้ LoginPage ตรงกับหน้าล็อคอินของคุณ
+    MaterialPageRoute(builder: (context) => LoginPage()),
     (Route<dynamic> route) => false,
   );
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late User? user;
+  late List<dynamic> groups;
+  String selectedSubject = 'Math';
+  @override
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.currentUser;
+    groups = [];
+    if (user != null) {
+      fetchGroups();
+    }
+  }
+
+  void fetchGroups() async {
+    QuerySnapshot querySnapshot = await _firestore
+        .collection('groups')
+        .where('createdBy', isEqualTo: user!.uid)
+        .get();
+    setState(() {
+      groups = querySnapshot.docs;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,126 +66,135 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: ListView.builder(
+        itemCount: groups.length,
+        itemBuilder: (BuildContext context, int index) {
+          var group = groups[index];
+          return ListTile(
+            title: Text(group['groupName']),
+            subtitle: Text(group['selectedSubject']),
+            onTap: () {
+              // รหัสดำเนินการเมื่อกดที่รายการ
+            },
+          );
+        },
       ),
     );
   }
-}
 
-void _showCreateGroupDialog(BuildContext context) {
-  List<String> subjects = [
-    'Math',
-    'Science',
-    'History',
-    'English',
-    'Art',
-    'Programing',
-  ]; // Replace this list with your subjects
+  void _showCreateGroupDialog(BuildContext context) {
+    List<String> subjects = [
+      'Math',
+      'Science',
+      'History',
+      'English',
+      'Art',
+      'Programing',
+    ];
 
-  String selectedSubject = subjects[0];
-  String groupName = '';
-  String description = '';
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Create Group'),
-        content: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            TextField(
-              onChanged: (value) {
-                groupName = value; // อัปเดตค่า groupName เมื่อมีการเปลี่ยนแปลง
-              },
-              decoration: InputDecoration(
-                labelText: 'Group Name',
-                hintText: 'Enter the name of your group',
-              ),
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Text('Subject: '),
-                SizedBox(width: 10),
-                DropdownButton<String>(
-                  value: selectedSubject,
-                  onChanged: (String? newValue) {
-                    selectedSubject = newValue!;
-                  },
-                  items: subjects.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
+    String groupName = '';
+    String description = '';
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Create Group'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    groupName = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Group Name',
+                  hintText: 'Enter the name of your group',
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            TextField(
-              onChanged: (value) {
-                description =
-                    value; // อัปเดตค่า description เมื่อมีการเปลี่ยนแปลง
-              },
-              decoration: InputDecoration(
-                labelText: 'Description',
-                hintText: 'Enter a brief description of your group',
               ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Text('Subject: '),
+                  SizedBox(width: 10),
+                  DropdownButton<String>(
+                    value: selectedSubject,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedSubject = newValue!;
+                      });
+                    },
+                    items:
+                        subjects.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    description = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  hintText: 'Enter a brief description of your group',
+                ),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                addDataToFirestore(groupName, selectedSubject, description);
+                Navigator.of(context).pop();
+              },
+              child: Text('Create'),
             ),
           ],
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('Cancel'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              addDataToFirestore(groupName, selectedSubject,
-                  description); // Replace 'user123' with the actual user ID
-              Navigator.of(context).pop();
-            },
-            child: Text('Create'),
-          ),
-        ],
-      );
-    },
-  );
-}
+        );
+      },
+    );
+  }
 
-void addDataToFirestore(
-    String groupName, String selectedSubject, String description) async {
-  User? user = FirebaseAuth.instance.currentUser;
-  if (user != null) {
-    String userId = user.uid;
+  void addDataToFirestore(
+      String groupName, String selectedSubject, String description) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      String userId = user.uid;
 
-    // สร้าง Group ID แบบสุ่ม
-    var random = Random();
-    String groupId = '';
-    for (var i = 0; i < 6; i++) {
-      groupId += (random.nextInt(10)).toString();
-    }
+      var random = Random();
+      String groupId = '';
+      for (var i = 0; i < 6; i++) {
+        groupId += (random.nextInt(10)).toString();
+      }
 
-    // เพิ่มข้อมูลลงใน Firestore
-    try {
-      await _firestore.collection('groups').doc(groupId).set({
-        'groupId': groupId,
-        'groupName': groupName,
-        'selectedSubject': selectedSubject,
-        'description': description,
-        'createdBy': userId,
-        'createdAt': FieldValue.serverTimestamp(), // วันที่และเวลาที่สร้าง
-        'members': [], // สมาชิกในกลุ่มที่จะเพิ่มเข้ามาทีหลัง
-      });
-    } catch (e) {
-      print('เกิดข้อผิดพลาดในการเพิ่มข้อมูล: $e');
+      try {
+        await _firestore.collection('groups').doc(groupId).set({
+          'groupId': groupId,
+          'groupName': groupName,
+          'selectedSubject': selectedSubject,
+          'description': description,
+          'createdBy': userId,
+          'createdAt': FieldValue.serverTimestamp(),
+          'members': [],
+        });
+      } catch (e) {
+        print('Error adding data: $e');
+      }
     }
   }
 }
